a675a05f1f6f9f802db99209cc09f5603c46e78a
Piotr Krzeminski <git@krzeminski.it>
2019-12-21 23:31:59 +0100

    Add code for Day 1

---
+++ src/main/kotlin/day1.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val moduleMasses = {}.javaClass.getResource("inputs/day1").readText().split("\n").map(String::toInt)
+    println(fuelForModulesNotTakingFuelMassIntoConsideration(moduleMasses))
+    println(fuelForModulesTakingFuelMassIntoConsideration(moduleMasses))
+}
+
+fun fuelForModulesNotTakingFuelMassIntoConsideration(moduleMasses: List<Int>) =
+    moduleMasses
+        .map { fuelForMass(it) }
+        .sum()
+
+fun fuelForModulesTakingFuelMassIntoConsideration(moduleMasses: List<Int>) =
+    moduleMasses
+        .map { fuelForMassRecursive(it) }
+        .sum()
+
+tailrec fun fuelForMassRecursive(mass: Int, accumulator: Int = 0): Int {
+    val firstLevelFuelMass = fuelForMass(mass)
+
+    if (firstLevelFuelMass <= 0) {
+        return accumulator
+    }
+
+    return fuelForMassRecursive(firstLevelFuelMass, firstLevelFuelMass + accumulator)
+}
+
+fun fuelForMass(mass: Int) =
+    mass/3 - 2
